b5b00db6ed8eeac9e00a04c66556714480819f58
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2025-02-12 15:51:57 +0800

    4.6 author with most blogs helper function and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
@@ -11,8 +13,24 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null
+
+    const authorCounts = _.countBy(blogs, 'author')
+    console.log('authorCounts',authorCounts)
+
+    const topAuthor = _.maxBy(_.keys(authorCounts), (author) => authorCounts[author]);
+    console.log('topAuthor', topAuthor)
+
+    return {
+        author: topAuthor,
+        blogs: authorCounts[topAuthor]
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
