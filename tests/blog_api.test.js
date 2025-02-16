ad6f0d993eaeee45b4ed2f7f6e6596c2b1a68a66
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2025-02-16 17:08:53 +0800

    add step 2 and step 3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after , beforeEach } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -18,16 +18,52 @@ beforeEach(async () => {
         .map(blog => new Blog(blog))
     const promiseArray = blogsObjects.map(blog => blog.save())
     await Promise.all(promiseArray)
-    
+
 })
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
 
+test('verify that the unique identifier property of the blog posts is named id', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    response.body.forEach(blog => {
+        assert.ok(blog.id !== undefined, 'Each blog should have an "id" property');
+        assert.strictEqual(typeof blog.id, 'string', 'The "id" property should be a string');
+    });
+})
+
+test.only('successful post adds one to the blog', async () => {
+    const newBlog =  {
+        "title": "The new test blog",
+        "author": "Gilbert Testmaster",
+        "url": "test.com",
+        "likes": 999,
+        "id": "67ab1b7b9d0cc4568f65e311"
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+    assert(contents.includes('The new test blog'))
+
+})
 
 after(async () => {
     await mongoose.connection.close()
