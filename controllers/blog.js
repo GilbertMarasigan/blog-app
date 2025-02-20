4bdfbd4d47d1a45ff5c50b56c46908f8bc2da503
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2025-02-20 20:38:07 +0800

    add user token auth and middleware to retrieve user from token

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,29 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
-const { request, response } = require('express')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const protect = async (request, response, next) => {
+    let token;
+
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = request.headers.authorization.split(' ')[1]; // Get token after 'Bearer'
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.SECRET);
+
+            // 🔹 Await should now work properly
+            request.user = await User.findById(decoded.id).select('-password');
+
+            next(); // Ensure the request moves to the next middleware/route
+        } catch (error) {
+            return response.status(401).json({ error: 'Not authorized, invalid token' });
+        }
+    } else {
+        return response.status(401).json({ error: 'No token, authorization denied' });
+    }
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -26,14 +49,25 @@ blogsRouter.patch('/:id', async (request, response) => {
 
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', protect, (request, response, next) => {
+
     const body = request.body
+
+    if (!request.user) {
+        return response.status(401).json({ error: 'User not authenticated' })
+    }
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes || 0,
+        user: {
+            username: request.user.username,
+            name: request.user.name,
+            id: request.user._id
+        }
+      
     })
 
     blog.likes = blog.likes === undefined ? 0 : blog.likes
@@ -42,12 +76,14 @@ blogsRouter.post('/', (request, response, next) => {
         response.status(400).json('Bad request')
     }
 
+
     blog
         .save()
         .then(savedBlog => {
             response.status(201).json(savedBlog)
         })
         .catch(error => next(error))
+
 })
 
 module.exports = blogsRouter
