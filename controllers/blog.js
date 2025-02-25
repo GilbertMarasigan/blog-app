d48f84125251f3894f8c1ce64de45b4dacaa09b3
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2025-02-25 15:19:53 +0800

    like and put request

--- controllers/blog.js
+++ controllers/blog.js
@@ -51,6 +51,20 @@ blogsRouter.patch('/:id', async (request, response) => {
 
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+
+    const { title, author, url, likes } = request.body
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        { title, author, url, likes },
+        { new: true, runValidators: true }
+    )
+
+    response.status(200).json(updatedBlog)
+
+})
+
 blogsRouter.post('/', async (request, response, next) => {
     try {
         const token = getTokenFrom(request)
